40bc23cf99d392876192984c442bbea88615a53b
Thompson207A <64737753+Thompson207A@users.noreply.github.com>
2020-05-17 18:23:37 +0100

    hunger and warmth bar code setup, also some death/victory functions

--- main.ts
+++ main.ts
@@ -118,70 +118,105 @@ e e e e e 2 2 2 2 2 2 2 e e e e
 e e e e e e e e e e e e e e e e 
 `
 }
-function createjohn () {
-    john = sprites.create(img`
-. . . . . . . . . . b 5 b . . . 
-. . . . . . . . . b 5 b . . . . 
-. . . . . . b b b b b b . . . . 
-. . . . . b b 5 5 5 5 5 b . . . 
-. . . . b b 5 d 1 f 5 5 d f . . 
-. . . . b 5 5 1 f f 5 d 4 c . . 
-. . . . b 5 5 d f b d d 4 4 . . 
-. b b b d 5 5 5 5 5 4 4 4 4 4 b 
-b d d d b b d 5 5 4 4 4 4 4 b . 
-b b d 5 5 5 b 5 5 5 5 5 5 b . . 
-c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
-c b d c d 5 5 b 5 5 5 5 5 5 b . 
-. c d d c c b d 5 5 5 5 5 d b . 
-. . c b d d d d d 5 5 5 b b . . 
-. . . c c c c c c c c b b . . . 
-. . . . . . . . . . . . . . . . 
-`, SpriteKind.Player)
-    controller.moveSprite(john)
-    initHUDSprite(freezeBar)
-    scene.cameraFollowSprite(john)
-}
-function drawHUDMeter (percent: number, hudSprite: Sprite, onColor: number, offColor: number) {
-    hudSprite.image.fill(offColor)
-    fillWidth = percent * meterWidth / 100
-    hudSprite.image.fillRect(0, 0, fillWidth, hudSprite.height, onColor)
-}
-function initHUDSprite (hudSprite: Sprite) {
+function initial_HUD (hudSprite: Sprite) {
     hudSprite.z = 200
     hudSprite.setFlag(SpriteFlag.RelativeToCamera, true)
     hudSprite.setImage(image.create(meterWidth, 4))
     hudSprite.left = 10
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    Hpercent += 10
+    if (Hpercent > 100) {
+        Hpercent = 100
+    }
+    otherSprite.destroy(effects.disintegrate, 500)
+})
+function createplayer () {
+    survival_guy = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . f f f f f f f f f . . . . . . . 
+. . . . . . . . f d d d d d d d f . . . . . . . 
+. . . . . . . . f d 1 f d 1 f d f . . . . . . . 
+. . . . . . . . f d d d d d d f f . . . . . . . 
+. . . . . . . . f f d f f d f f . . . . . . . . 
+. . . . . . . . . f d d d f f . . . . . . . . . 
+. . . . . . . . . f f f f . . . . . . . . . . . 
+. . . . . . . . . . . f f . . . . . . . . . . . 
+. . . . . . . . . . . f f . . . . . . . . . . . 
+. . . . . . . . f f f f f f f f . . . . . . . . 
+. . . . . . f f f . . f f . . f f f . . . . . . 
+. . . . . f f . . . . f f . . . . f f . . . . . 
+. . . . f f . . . . . f f . . . . . f f . . . . 
+. . . . . . . . . . . f f . . . . . . f . . . . 
+. . . . . . . . . . . f f . . . . . . . . . . . 
+. . . . . . . . . . . f f . . . . . . . . . . . 
+. . . . . . . . . f f f f f f . . . . . . . . . 
+. . . . . . . . f f . . . . f . . . . . . . . . 
+. . . . . . . f f . . . . . f f . . . . . . . . 
+. . . . . . . f . . . . . . . f f . . . . . . . 
+. . . . . . . f . . . . . . . . f . . . . . . . 
+. . . . . . f . . . . . . . . . f f . . . . . . 
+. . . . . . f . . . . . . . . . . f f . . . . . 
+`, SpriteKind.Player)
+    controller.moveSprite(survival_guy)
+    scene.cameraFollowSprite(survival_guy)
+}
+function forthebar (percent: number, hudSprite: Sprite, onColor: number, offColor: number) {
+    hudSprite.image.fill(offColor)
+    fillWidth = percent * meterWidth / 100
+    hudSprite.image.fillRect(0, 0, fillWidth, hudSprite.height, onColor)
+}
+function d (percent: number, hudSprite: Sprite, onColor: number, offColor: number) {
+	
+}
+let edible_thing: Sprite = null
+let distancefromheat = 0
 let fillWidth = 0
+let Hpercent = 0
 let meterWidth = 0
-let freezeBar: Sprite = null
-let john: Sprite = null
+let survival_guy: Sprite = null
 tiles.setTilemap(tiles.createTilemap(
-            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            hex`2000200007070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070708080808080404040404040707070707070707070707070707070707070707080808080804040404040404040808080807070707070707070707070707070708080808080404040404040404040808080807070707070707070707070707080808080808080404040404040404080808080807070707070707070707070708080808080808080404040404040408080808080807070707070707080808080808080808080808080404040408080808080808080707070707070708080808080808080808080808080808080808080808080808080707070707070808080808080808080808080808080808080808080808080808070707070707080808080808080808080808080808080808080808080808080808080707070708080808080808080808080808080808080808080808080808080808080807070808080808080808080808080808080808080808080808080808080808080707080808080808080808080808080808080808080808080808080808080808070708080808080808080808080808080808080808080808080808080808080807070808080808080808080808080808080808080808080808080808080808080707080808080808080808080808080805080808080808080808080808070707070708080808080808080808080808080808080808080808080808080807070707070808080808080808080808080808080808080808080808080808080707070707080808080808080808080808080808080808080808080808080808070707070708080808080808080808080808080808080808080808080808080807070707070708080808080808080808080808080808080808080808080808080707070707070707080808080808080808080808080808080808080808080808070707070707070703030308080808080808080808080808080808080808080807070707070707070303030308080808080808080808080808080808080808080707070707070707030303030303030808080808080808080808080808080807070707070707070703030303030303080808080808080808080808080808080707070707070707070703030303030303080808080808080808080808080808070707070707070707070303030303030308080808080808070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707`,
             img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 . . . . . . . . . . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 2 2 2 
+2 2 2 2 . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 2 2 
+2 2 2 2 . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 
+. . . . . . . . . . . . . . 2 . . . . . . . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 
+2 2 2 . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 
+2 2 2 . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 
+2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 2 2 
+2 2 2 2 . . . . . . . . . . . . . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,sprites.castle.tileGrass2,sprites.builtin.brick,sprites.castle.tilePath5],
             TileScale.Sixteen
         ))
-createjohn()
-tiles.placeOnTile(john, tiles.getTileLocation(16, 16))
-freezeBar = sprites.create(img`
+createplayer()
+tiles.placeOnTile(survival_guy, tiles.getTileLocation(16, 16))
+let heatbar = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -199,7 +234,7 @@ freezeBar = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.HUD)
-let hungerBar = sprites.create(img`
+let hungerbar = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -217,13 +252,11 @@ let hungerBar = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.HUD)
-hungerBar.top = scene.screenHeight() - 12
-freezeBar.top = scene.screenHeight() - 6
+hungerbar.top = scene.screenHeight() - 12
+heatbar.top = scene.screenHeight() - 6
 meterWidth = scene.screenWidth() - 20
-initHUDSprite(freezeBar)
-initHUDSprite(hungerBar)
-let hunger = 100
-let freeze = 100
+Hpercent = 100
+let heatpercent = 100
 let campfire = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -242,4 +275,40 @@ let campfire = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Fire)
-tiles.placeOnRandomTile(campfire, myTiles.tile3)
+tiles.placeOnRandomTile(campfire, myTiles.tile5)
+game.onUpdateInterval(500, function () {
+    distancefromheat = Math.sqrt((survival_guy.x - campfire.x) ** 2 + (survival_guy.y - campfire.y) ** 2)
+    if (distancefromheat < 48) {
+        heatpercent += 1
+        if (heatpercent > 100) {
+            heatpercent = 100
+        }
+    } else {
+        heatpercent += -1
+    }
+    Hpercent += -1
+    d(heatpercent, heatbar, 7, 1)
+    d(Hpercent, hungerbar, 3, 1)
+})
+game.onUpdateInterval(5000, function () {
+    edible_thing = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . 2 2 2 2 2 2 2 . . . . . 
+. . . 1 2 2 2 2 2 2 2 1 . . . . 
+. . 1 1 2 2 1 1 2 2 2 1 1 . . . 
+. 2 2 2 2 2 1 1 2 2 2 2 2 2 . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+. 1 1 2 2 2 2 2 2 1 1 2 2 2 . . 
+. 1 1 2 2 2 2 2 2 1 1 2 2 2 . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . b d d d b . . . . . . 
+. . . . . . b b b . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Food)
+    tiles.placeOnRandomTile(edible_thing, myTiles.tile1)
+    edible_thing.lifespan = 10000
+})
